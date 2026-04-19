e6845b5b975431cb549f396244c4ffa8b8a0fcd4
jesjohn-hue <selormtetedje@gmail.com>
2026-04-18 19:43:53 -0500

    Move Google Maps JavaScript out of HTML content and clean map page structure

--- js/script.js
+++ js/script.js
@@ -1,41 +1,13 @@
-console.log("Project 3 JavaScript loaded successfully.");
-
 function initMap() {
   const selectedLocation = { lat: 41.9981, lng: -87.6556 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
     center: selectedLocation,
-    zoom: 15,
-    mapTypeId: "roadmap",
-    zoomControl: true,
-    streetViewControl: true,
-    fullscreenControl: true
-  });
-
-  const infoWindow = new google.maps.InfoWindow({
-    content: `
-      <div>
-        <h3>Loyola University Chicago</h3>
-        <p>This is my selected location for Project 3.</p>
-      </div>
-    `
+    zoom: 14
   });
 
   const marker = new google.maps.Marker({
     position: selectedLocation,
-    map: map,
-    title: "Selected Location"
-  });
-
-  marker.addListener("click", function () {
-    infoWindow.open(map, marker);
-  });
-
-  // FEATURE 3: click anywhere to drop marker
-  map.addListener("click", function (event) {
-    new google.maps.Marker({
-      position: event.latLng,
-      map: map
-    });
+    map: map
   });
 }
